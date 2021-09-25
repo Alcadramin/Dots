f94d424aff758744e3ecf63e987887ce515464c2
Berkcan Uçan <berkcan@vivaldi.net>
2021-09-25 16:42:53 +0300

    🖼 Added wallpapers, polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar//"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+BAR_NAME=main
+BAR_CONFIG=/home/$USER/.config/polybar/config.ini
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on primary monitor
+MONITOR=$PRIMARY polybar --reload -c $BAR_CONFIG $BAR_NAME &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar --reload -c $BAR_CONFIG $BAR_NAME &
+done
+
+# Launch the bar
+# polybar -q main -c "$DIR"/config.ini &
