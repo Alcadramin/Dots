1820f5724b6ab8b584143c245ed0249f9f24525c
Berkcan Uçan <hello@alca.dev>
2022-05-27 22:13:06 +0300

    📦 updated emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -7,42 +7,40 @@
 ;; Licensed under the MIT License (MIT).
 ;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;      GENERAL CONFIGS & COMPANY
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq user-full-name "Berkcan Ucan"
+;; Personal info
+(setq user-full-name "Berkcan Uçan"
       user-mail-address "hello@alca.dev")
 
-(setq doom-theme 'doom-snazzy)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;      CUSTOMIZATION
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; JetBrains Mono
-;(setq doom-font
-;      (font-spec :family "JetBrainsMono Nerd Font" :size 14 :weight 'bold)
-;      doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 15)
-;      doom-big-font (font-spec :family "JetBrainsMono Nerd Font" :size 24))
+(setq doom-theme 'doom-tokyo-night)
 
-;; Victor Mono
+;; Set Font
 (setq doom-font
-      (font-spec :family "VictorMono Nerd Font" :size 14 :weight 'bold)
-      doom-variable-pitch-font (font-spec :family "VictorMono Nerd Font" :size 15)
-      doom-big-font (font-spec :family "VictorMono Nerd Font" :size 24))
-
+      (font-spec :family "Victor Mono" :size 13 :weight 'bold)
+      doom-variable-pitch-font (font-spec :family "Victor Mono" :size 13)
+      doom-big-font (font-spec :family "Victor Mono" :size 16))
 
 (after! doom-themes
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
 
-;; Make comments and keywords Italic.
+;; Make comments etc. italic
 (custom-set-faces!
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
 
-(setq org-directory "~/Documents/Org/")
-
 (setq display-line-numbers-type t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;      AUTO-COMPLETE (Company)
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Format on save.
 (setq +format-on-save-enabled-modes
@@ -52,15 +50,17 @@
             latex-mode))
 
 ;; Company stuff
-(use-package company
-  :init
-  (add-hook 'after-init-hook 'global-company-mode))
-
-;; Company with TabNine
-(use-package company-tabnine :ensure t)
+;(use-package company-tabnine :ensure t)
+(require 'company-tabnine)
 (setq company-idle-delay 0)
 (setq company-show-quick-access t)
 (add-to-list 'company-backends #'company-tabnine)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;      KEYBINDINGS
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Line keybindings
 (setq display-line-numbers-type t)
@@ -72,117 +72,118 @@
        :desc "Toggle line highlight globally" "H" #'global-hl-line-mode
        :desc "Toggle truncate lines" "t" #'toggle-truncate-lines))
 
-;; Setting "SPC + - -> a" to open Org Agenda
-(map! :leader
-      (:prefix ("-" . "open file")
-       :desc "Edit agenda file" "a" #'(lambda () (interactive) (find-file "~/Documents/Org/agenda.org"))))
-
 ;; Setting "SPC + d -> t" to enable treemacs-mode
 (map! :leader
       (:prefix ("d". "Treemacs")
        :desc "Treemacs mode" "t" 'treemacs))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;      MISC CONFIG (Disabling most of it for now, it's getting bloat :P)
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Enable rainbow mode
 (add-hook 'css-mode-hook #'rainbow-mode)
 
 ;; PDF Minor Mode
-(use-package pdf-view
-  :hook (pdf-tools-enabled . pdf-view-midnight-minor-mode)
-  :hook (pdf-tools-enabled . hide-mode-line-mode)
-  :config
-  (setq pdf-view-midnight-colors '("#eff0eb" . "#282a36")))
+;(use-package pdf-view
+;  :hook (pdf-tools-enabled . pdf-view-midnight-minor-mode)
+;  :hook (pdf-tools-enabled . hide-mode-line-mode)
+;  :config
+;  (setq pdf-view-midnight-colors '("#eff0eb" . "#282a36")))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;      CENTAUR-TABS
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq centaur-tabs-set-bar 'over
-      centaur-tabs-set-icons t
-      centaur-tabs-gray-out-icons 'buffer
-      centaur-tabs-height 24
-      centaur-tabs-set-modified-marker t
-      centaur-tabs-style "bar"
-      centaur-tabs-modified-marker "•")
-(map! :leader
-      :desc "Toggle tabs globally" "t c" #'centaur-tabs-mode
-      :desc "Toggle tabs local display" "t C" #'centaur-tabs-local-mode)
-(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
-                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
-                                               (kbd "g <down>")  'centaur-tabs-forward-group
-                                               (kbd "g <up>")    'centaur-tabs-backward-group)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;      DASHBOARD
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(use-package dashboard
-  :init        (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-banner-logo-title "Doom Emacs")
-  (setq dashboard-startup-banner "/home/alcadramin/.doom.d/black_hole.png")
-  (setq dashboard-center-content t) ;; set to 't' for centered content
-  (setq dashboard-items '((recents . 5)
-                          (bookmarks . 5)
-                          (projects . 5)
-                          (agenda . 5 )))
-  :config
-  (dashboard-setup-startup-hook)
-  (dashboard-modify-heading-icons '((recents . "file-text")
-                                (bookmarks . "book"))))
-
-(setq doom-fallback-buffer "*dashboard*")
-(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))) ;; To run with emacs daemon.
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;      DIRED
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org mode stuff
+(setq org-directory "~/Org/")
 
 (map! :leader
-      (:prefix ("d" . "dired") ;; Map dired to "SPC + d -> d"
-       :desc "Open dired" "d" #'dired
-       :desc "Dired jump to current" "j" #'dired-jump)
-      (:after dired
-       (:map dired-mode-map
-        :desc "Peep-dired image previews" "d p" #'peep-dired
-        :desc "Dired view file" "d v" #'dired-view-file)))
+      (:prefix ("-" . "open file")
+       :desc "Edit agenda file" "a" #'(lambda () (interactive) (find-file "~/Documents/Org/agenda.org"))))
 
-(evil-define-key 'normal dired-mode-map
-  (kbd "M-RET") 'dired-display-file
-  (kbd "h") 'dired-up-directory
-  (kbd "l") 'dired-open-file ; use dired-find-file instead of dired-open.
-  (kbd "m") 'dired-mark
-  (kbd "t") 'dired-toggle-marks
-  (kbd "u") 'dired-unmark
-  (kbd "C") 'dired-do-copy
-  (kbd "D") 'dired-do-delete
-  (kbd "J") 'dired-goto-file
-  (kbd "M") 'dired-chmod
-  (kbd "O") 'dired-chown
-  (kbd "P") 'dired-do-print
-  (kbd "R") 'dired-rename
-  (kbd "T") 'dired-do-touch
-  (kbd "Y") 'dired-copy-filenamecopy-filename-as-kill
-  (kbd "+") 'dired-create-directory
-  (kbd "-") 'dired-up-directory
-  (kbd "% l") 'dired-downcase
-  (kbd "% u") 'dired-upcase
-  (kbd "; d") 'epa-dired-do-decrypt
-  (kbd "; e") 'epa-dired-do-encrypt)
-(evil-define-key 'normal peep-dired-mode-map
-  (kbd "j") 'peep-dired-next-file
-  (kbd "k") 'peep-dired-prev-file)
-(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
-(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
-(setq dired-open-extensions '(("gif" . "feh")
-                              ("jpg" . "feh")
-                              ("png" . "feh")
-                              ("mkv" . "mpv")
-                              ("mp4" . "mpv")))
+;(setq centaur-tabs-set-bar 'over
+;      centaur-tabs-set-icons t
+;      centaur-tabs-gray-out-icons 'buffer
+;      centaur-tabs-height 24
+;      centaur-tabs-set-modified-marker t
+;      centaur-tabs-style "bar"
+;      centaur-tabs-modified-marker "•")
+;(map! :leader
+;      :desc "Toggle tabs globally" "t c" #'centaur-tabs-mode
+;      :desc "Toggle tabs local display" "t C" #'centaur-tabs-local-mode)
+;(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
+;                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
+;                                               (kbd "g <down>")  'centaur-tabs-forward-group
+;                                               (kbd "g <up>")    'centaur-tabs-backward-group)
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;      DASHBOARD
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;(use-package dashboard
+;  :init        (setq dashboard-set-heading-icons t)
+;  (setq dashboard-set-file-icons t)
+;  (setq dashboard-banner-logo-title "Doom Emacs")
+;  (setq dashboard-startup-banner "/home/alcadramin/.doom.d/black_hole.png")
+;  (setq dashboard-center-content t) ;; set to 't' for centered content
+;  (setq dashboard-items '((recents . 5)
+;                          (bookmarks . 5)
+;                          (projects . 5)
+;                          (agenda . 5 )))
+;  :config
+;  (dashboard-setup-startup-hook)
+;  (dashboard-modify-heading-icons '((recents . "file-text")
+;                                (bookmarks . "book"))))
+;
+;(setq doom-fallback-buffer "*dashboard*")
+;(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))) ;; To run with emacs daemon.
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;      DIRED
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;(map! :leader
+;      (:prefix ("d" . "dired") ;; Map dired to "SPC + d -> d"
+;       :desc "Open dired" "d" #'dired
+;       :desc "Dired jump to current" "j" #'dired-jump)
+;      (:after dired
+;       (:map dired-mode-map
+;        :desc "Peep-dired image previews" "d p" #'peep-dired
+;        :desc "Dired view file" "d v" #'dired-view-file)))
+;
+;(evil-define-key 'normal dired-mode-map
+;  (kbd "M-RET") 'dired-display-file
+;  (kbd "h") 'dired-up-directory
+;  (kbd "l") 'dired-open-file ; use dired-find-file instead of dired-open.
+;  (kbd "m") 'dired-mark
+;  (kbd "t") 'dired-toggle-marks
+;  (kbd "u") 'dired-unmark
+;  (kbd "C") 'dired-do-copy
+;  (kbd "D") 'dired-do-delete
+;  (kbd "J") 'dired-goto-file
+;  (kbd "M") 'dired-chmod
+;  (kbd "O") 'dired-chown
+;  (kbd "P") 'dired-do-print
+;  (kbd "R") 'dired-rename
+;  (kbd "T") 'dired-do-touch
+;  (kbd "Y") 'dired-copy-filenamecopy-filename-as-kill
+;  (kbd "+") 'dired-create-directory
+;  (kbd "-") 'dired-up-directory
+;  (kbd "% l") 'dired-downcase
+;  (kbd "% u") 'dired-upcase
+;  (kbd "; d") 'epa-dired-do-decrypt
+;  (kbd "; e") 'epa-dired-do-encrypt)
+;(evil-define-key 'normal peep-dired-mode-map
+;  (kbd "j") 'peep-dired-next-file
+;  (kbd "k") 'peep-dired-prev-file)
+;(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
+;(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+;(setq dired-open-extensions '(("gif" . "feh")
+;                              ("jpg" . "feh")
+;                              ("png" . "feh")
+;                              ("mkv" . "mpv")
+;                              ("mp4" . "mpv")))
+
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -4,8 +4,8 @@
 ;; in. Remember to run 'doom sync' after modifying it!
 
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find a "Module Index" link where you'll find
-;;      a comprehensive list of Doom's modules and what flags they support.
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
 
 ;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
 ;;      'C-c c k' for non-vim users) to view its documentation. This works on
@@ -15,6 +15,7 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
+       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
@@ -29,13 +30,13 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       ;;doom-dashboard    ; a nifty splash screen for Emacs
+       doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)  ; 🙂
+       ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       (ligatures +extras)        ; ligatures and symbols to make your code pretty again
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
@@ -47,7 +48,7 @@
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       ;;window-select     ; visually switch windows
+       window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing
 
@@ -66,7 +67,7 @@
        word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons)             ; making dired pretty [functional]
+       dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
@@ -76,28 +77,29 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       (spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
+       ;;biblio            ; Writes a PhD for you (citation needed)
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       gist              ; interacting with github gists
+       ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       ;;pdf               ; pdf enhancements
        prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
@@ -112,11 +114,11 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       cc                ; C > C++ == 1
+       (cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
-       crystal           ; ruby at the speed of c
+       ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
@@ -128,28 +130,30 @@
        ;;ess               ; emacs speaks statistics
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
+       ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       ;;(go +lsp)         ; the hipster dialect
-       (haskell +dante)  ; a language that's lazier than I am
+       (go +lsp)         ; the hipster dialect
+       (graphql +lsp)    ; Give queries a REST
+       (haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
-       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       julia             ; a better, faster MATLAB
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
+       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+       ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        org               ; organize your plain life in plain text
-       ;;php               ; perl's insecure younger brother
+       php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
@@ -158,8 +162,8 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
